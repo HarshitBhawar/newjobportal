5c6536cebf1303d7b97ae29c45c4ec2cf527d1d5
Your Name <you@example.com>
2022-09-28 12:17:42 +0530

    confirm

--- app/controllers/users/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class UsersController < ApplicationController
-    def index
-     UserMailer.example(User.new(email: 'bo@samurails.com')).deliver
-     @users = User.all
-    end
-    
-end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "harry@example.com"
   layout "mailer"
 end
